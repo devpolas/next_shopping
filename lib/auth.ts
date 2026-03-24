14ac35823e81a31c3fd2511b1dbee4908397db26
devpolas <195308698+devpolas@users.noreply.github.com>
2026-03-24 11:30:07 +0600

    first commit with setup better-auth and prisma with neondb

---
+++ lib/auth.ts
@@ -0,0 +1,19 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./db";
+
+export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
+  secret: process.env.BETTER_AUTH_SECRET,
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+    requireEmailVerification: false,
+  },
+
+  session: {
+    expiresIn: 60 * 60 * 24 * 7, // 7 days
+  },
+});
