f27cb0747a938c2a2f1efad3a2997d20e98a7fe0
devpolas <195308698+devpolas@users.noreply.github.com>
2026-03-27 01:43:06 +0600

    fix prerender

--- app/(auth)/verify-account/page.tsx
+++ app/(auth)/verify-account/page.tsx
@@ -9,7 +9,7 @@ import {
 import { Button } from "@/components/ui/button"; // if you have a button component
 import { sendVerificationEmail } from "@/lib/actions/auth.actions";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import { toast } from "sonner";
 import LoadingSpinner from "@/components/spinner/loading-spinner";
 
@@ -53,17 +53,19 @@ export default function VerifyEmail() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <Button
-          onClick={handleForgetPassword}
-          disabled={loading}
-          variant={"outline"}
-        >
-          {loading ? (
-            <LoadingSpinner text='sending...' />
-          ) : (
-            <span>Resend Verification Email</span>
-          )}
-        </Button>
+        <Suspense fallback={<LoadingSpinner text='loading...' />}>
+          <Button
+            onClick={handleForgetPassword}
+            disabled={loading}
+            variant={"outline"}
+          >
+            {loading ? (
+              <LoadingSpinner text='sending...' />
+            ) : (
+              <span>Resend Verification Email</span>
+            )}
+          </Button>
+        </Suspense>
       </CardContent>
     </Card>
   );
